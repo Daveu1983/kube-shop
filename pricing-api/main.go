68647e83c89a27b03ea7ba2766b1010e67f7dace
David <dunderwood13@hotmail.co.uk>
2025-04-13 09:09:18 +0100

    extracted ports in pricing api to env variable

--- pricing-api/main.go
+++ pricing-api/main.go
@@ -4,30 +4,33 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+    "os"
 )
 
 func main(){
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "5000" // Default value
+    }
     http.HandleFunc("/api",func(w http.ResponseWriter, r *http.Request){
-    queryParameters := r.URL.Query()
-    price := queryParameters.Get("price")
-    if floatPrice, err := strconv.ParseFloat(price, 64); err ==nil {
-        title := queryParameters.Get("title")       
-        newPrice := getPrice(floatPrice, title)
-        jsonPrice, err := json.Marshal(newPrice)
-        if err != nil {
+        queryParameters := r.URL.Query()
+        price := queryParameters.Get("price")
+        if floatPrice, err := strconv.ParseFloat(price, 64); err ==nil {
+                title := queryParameters.Get("title")       
+                newPrice := getPrice(floatPrice, title)
+                jsonPrice, err := json.Marshal(newPrice)
+                if err != nil {
+                    http.Error(w, err.Error(), http.StatusInternalServerError)
+                    return
+                }
+                w.Header().Set("Content-Type", "application/json") 
+                w.Write(jsonPrice)
+        } else {
             http.Error(w, err.Error(), http.StatusInternalServerError)
             return
         }
-        w.Header().Set("Content-Type", "application/json") 
-        w.Write(jsonPrice)
-    } else {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    
-})
-http.ListenAndServe(":5000", nil)
+    })
+    http.ListenAndServe(":"+port, nil)
 }
 
 func getPrice(price float64, title string ) (float64) {
